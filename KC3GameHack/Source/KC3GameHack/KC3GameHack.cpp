0db3863c646acebfa06ccffcef3c79d5004aa727
Back370 <y.endo.203@stn.nitech.ac.jp>
2026-02-16 17:00:14 +0900

    初期設定

---
+++ KC3GameHack/Source/KC3GameHack/KC3GameHack.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KC3GameHack.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KC3GameHack, "KC3GameHack" );
+
+DEFINE_LOG_CATEGORY(LogKC3GameHack)
---
+++ KC3GameHack/Source/KC3GameHack/KC3GameHackGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KC3GameHackGameMode.h"
+
+AKC3GameHackGameMode::AKC3GameHackGameMode()
+{
+	// stub
+}
